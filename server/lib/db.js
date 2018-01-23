094aec436c88063b3aef7e71010e8d78242bc612
Azzorg <c.goupil72@gmail.com>
2018-01-23 12:11:02 -0500

    Mise en place du client/server

---
+++ server/lib/db.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var Order = new Schema({
+  id: { type: Number, unique: true },
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  products: Array
+}, { versionKey: false });
+
+
+var Product = new Schema({
+  id: { type: Number, unique: true },
+  name: String,
+  price: Number,
+  image: String,
+  category: String,
+  description: String,
+  features: Array
+}, { versionKey: false });
+
+mongoose.model("Order", Order);
+mongoose.model("Product", Product);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://admin:1a2b3c@ds255265.mlab.com:55265/online-shop-db');
+
+module.exports = Product;
+module.exports = Order;
+
